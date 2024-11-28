00f5fca8a99da9ce3b5ad4812c9bd46cec86f19a
RothJie <3110711143@qq.com>
2024-11-28 10:17:28 +0800

    学习math

---
+++ Study_math/Study_math.py
@@ -0,0 +1,16 @@
+import math
+
+value_pi = math.pi
+value_e = math.e
+sqrt_value = math.sqrt(25)
+
+# a_num = math.ceil(5) #向上取整，返回大于或等于x的最小整数。
+# a_num = math.floor(6) #向下取整，返回小于或等于x的最大整数。
+# a_num = math.pow(2,10) # 计算一个数x的y次幂
+# a_num = math.log2(8)  # 计算以2为底数8的对数
+# a_num = math.gcd(45,6) #返回给定的整数参数的最大公约数。
+
+print(value_pi)
+print(value_e)
+print(sqrt_value)
+print(a_num)
